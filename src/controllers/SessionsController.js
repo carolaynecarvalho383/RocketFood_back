0390690f243a5517797bd29d2b90af7dfee26298
luiszkm <luiszkm@gmail.com>
2023-02-21 12:12:23 -0300

    bug login is ok

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -12,12 +12,15 @@ class SessionsController{
     const user = await knex("users")
     .where({email})
     .first()
+
     
     if(!user){
       throw new AppError("E-mail e/ou senha incorreto", 401)
     }
 
-    const passwordMatched = await compare(password, user.password);
+  
+    const passwordMatched = await compare(String(password), user.password);
+
 
     if(!passwordMatched){
       throw new AppError("E-mail e/ou senha incorreto", 401)
